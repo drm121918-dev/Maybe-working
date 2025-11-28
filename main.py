7057862138e8bf4133c0bb917810542781a85690
drm121918-dev <drm121918@gmail.com>
2025-11-28 16:13:07 +0530

    Fix bot initialization syntax in main.py

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ import re
 import os
 
 bot = Client
-    "bot",
-    api_id=36925285,
-    api_hash=ef3e2c581370c93287854dc36d78c13c,
-    bot_token=8095739923:AAG7FBm_YIHxaN_QnUN6fXo8hFTOtEjSiAE,
+    "bot"
+    api_id=36925285
+    api_hash=ef3e2c581370c93287854dc36d78c13c
+    bot_token=8095739923:AAG7FBm_YIHxaN_QnUN6fXo8hFTOtEjSiAE
 
 @bot.on_message(filters.command(["stop"]))
 async def cancel_command(bot: Client, m: Message):
